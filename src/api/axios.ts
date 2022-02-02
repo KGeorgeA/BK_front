b83b3dad1f60a0c2b7d8f369fdcbe7341d74b088
KGeorgeA <karcivadzega@gmail.com>
2022-02-02 18:08:51 +0300

    Improved connection with server

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,4 +4,25 @@ const api = Axios.create({
   baseURL: "http://localhost:4000",
 });
 
+api.interceptors.request.use(async (url: any) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    url.headers.common.Authorization = `Bearer ${token}`;
+  } else {
+    url.headers.common.Authorization = '';
+  }
+
+  return url;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    // const isErrorStatus = error.response?.status === 400; // обработка должна быть здесь, но я дебил
+    // диспатчить валидационную ошибку)))
+    return error.response
+  }
+);
+
 export default api;
--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -13,11 +13,23 @@ export type UserSignupData = {
 };
 
 export const userSignin = async (data: UserSigninData) => {
-  const response = await api.post("/api/login", { data });
-  return response.data;
+  // console.log("api signin data", data);
+  const response = await api.post("/api/signin", { data });
+  // console.log("api signin response",response);
+
+  return response;
 };
 
 export const userSignup = async (data: UserSignupData) => {
-  const response = await api.post("/registration", { data });
-  return response.data;
+  // console.log("api signup data", data);
+  // const response = await api.post("/api/signup", { data });
+  const response = await api.post("/api/signup", { data });
+  // console.log("api signup response",response);
+
+  return response;
 };
+
+// export const userTokenCheck = async (data: any) => {
+//   const response = await api.post("/api/tokenCheck", { data });
+//   return response;
+// };
